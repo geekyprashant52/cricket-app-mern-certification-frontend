0a72f2d146a88d4afa67cdc1c0c782a28c7ff2a6
geekyprashant52 <61961514+geekyprashant52@users.noreply.github.com>
2021-10-18 18:09:40 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./Components/HomePage/HomePage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import TopBar from "./Components/TopBar/TopBar";
+import { useState } from "react";
+import TeamDetails from "./Components/TeamDetails/TeamDetails";
+import PlayerDetails from "./Components/PlayerDetails/PlayerDetails";
 
 function App() {
+  const [isOpen, setisOpen] = useState(false);
+  const shareDataToComponents = (isOpen) => {
+    setisOpen(isOpen);
+  };
   return (
     <div className="App">
-      <h1>Cricket App</h1>
-      <HomePage />
+      <BrowserRouter>
+        <TopBar shareDataToComponents={shareDataToComponents} isOpen={isOpen} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => (
+              <HomePage
+                isOpen={isOpen}
+                shareDataToComponents={shareDataToComponents}
+              />
+            )}
+          />
+          <Route path="/team/:team/:id" component={TeamDetails} />
+          <Route path="/player/:id" component={PlayerDetails} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
